db95ea0f22802e642364f1a515aa8372573eb19d
ag20373 <ag20373@gmail.com>
2024-12-31 20:20:28 +0530

    Add project files.

---
+++ DSA/1_Basics/1_FirstProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA._1_Basics
+{
+    class _1_FirstProgram
+    {
+        public _1_FirstProgram()
+        {
+            StringDisplayConstructor();
+        }
+        public static void StringDisplayDirectly(string Value)
+        {
+            Console.WriteLine(Value);
+        }
+        public  void StringDisplayConstructor()
+        {
+            Console.WriteLine("Called From Constructor");
+        }
+        public void StringDisplayObject(string Value)
+        {
+            Console.WriteLine(Value);
+        }
+    }   
+}
---
+++ DSA/Program.cs
@@ -0,0 +1,21 @@
+﻿using DSA._1_Basics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1_Basics
+            _1_FirstProgram.StringDisplayDirectly("Called Using Static");
+            _1_FirstProgram obj = new _1_FirstProgram();
+            obj.StringDisplayObject("Called Using Object");
+            #endregion
+        }
+    }
+}
